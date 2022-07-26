4ca5f965735651e6e66742d7f472689cfbbcd8ef
Sebastian Herrera <seba.herrera1985@gmail.com>
2022-07-26 19:22:22 -0300

    committed

---
+++ app.js
@@ -0,0 +1,10 @@
+let entrada = prompt("cuanto es 2 x 3', tenes tres oportunidades");
+
+
+
+while(entrada != "6"){
+    alert("El usuario ingresó "+ entrada);
+    
+    entrada = prompt("NO PAMPLIN!!! vamos de nuevo");
+
+}
